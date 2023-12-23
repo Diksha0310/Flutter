40fabd4c7cf07290ae2ed9474c86d2cc451ce541
Diksha Tiwari <dtdiksha03@gmail.com>
2023-12-23 21:19:11 +0530

    Day3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'pages/home.dart';
 
 void main()
 {
@@ -12,7 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch:Colors.deepPurple),
+      darkTheme: ThemeData(brightness:Brightness.dark,),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/login": (context) => HomePage(),
+      },
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,2 +1,16 @@
 import 'package:flutter/material.dart';
-q
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text("Login Page",
+        style: TextStyle(fontSize: 20,
+        color: Colors.blue,
+        fontWeight: FontWeight.bold),),
+      ),
+    );
+  }
+}
